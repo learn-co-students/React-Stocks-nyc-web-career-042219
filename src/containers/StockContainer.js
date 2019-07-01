6816e2f8eddc349b0a52502653a9c45c42e921f4
Jessica Lin <jessicaa.lin@gmail.com>
2019-07-01 19:01:30 -0400

    Done.

--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -4,11 +4,13 @@ import Stock from '../components/Stock'
 class StockContainer extends Component {
 
   render() {
+    // console.log(this.props)
     return (
       <div>
         <h2>Stocks</h2>
         {
           //render the list of stocks here
+          this.props.stocks.map( stock => < Stock stock={stock} handleClick={this.props.handleClick} />)
         }
       </div>
     );
