d2685b442508244382fe85ac835e67e20301e425
Won Kim <wonsae.kim@gmail.com>
2019-07-01 17:04:17 -0400

    Done.

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,20 +1,13 @@
 import React from 'react'
 
-const Stock = () => (
+const Stock = (props) => (
   <div>
-
-    <div className="card">
+    <div className="card" onClick={() => props.handleStock(props)}>
       <div className="card-body">
-        <h5 className="card-title">{
-            //Company Name
-          }</h5>
-        <p className="card-text">{
-            //ticker: stock price
-          }</p>
+        <h5 className="card-title">{props.name}</h5>
+        <p className="card-text">{props.price}</p>
       </div>
     </div>
-
-
   </div>
 );
 
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -4,22 +4,68 @@ import PortfolioContainer from './PortfolioContainer'
 import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
+  state = {
+    stocks: [],
+    portfolio: [],
+    filtered: [],
+    sortedBy: ''
+  }
 
+  componentDidMount() {
+    fetch('http://localhost:3000/stocks')
+    .then(r => r.json())
+    .then(data => {
+      this.setState({
+        stocks: data,
+        filtered: data
+      })
+    })
+  }
+
+  handleStock = (stock) => {
+    let targetStock = this.state.portfolio.find(s => s.id === stock.id)
+    if (targetStock === undefined) {
+      this.setState({
+        portfolio: [...this.state.portfolio, stock]
+      })
+    } else {
+      let updatedPortfolio = this.state.portfolio.filter(s => s.id !== stock.id)
+      this.setState({
+        portfolio: updatedPortfolio
+      })
+    }
+  }
+
+  handleSort = (e) => {
+    let filteredStocks = this.state.stocks.filter(s => s.type === e.target.value)
+    this.setState({
+      filtered: filteredStocks
+    })
+  }
+
+  sortBy = (type) => {
+    let sortedStocks = this.state.stocks
+    if (type === 'alpha') {
+      sortedStocks.sort((a, b) => (a.name < b.name ? -1 : (a.name > b.name ? 1 : 0)))
+    } else if (type === 'price') {
+      sortedStocks.sort((a, b) => b.price - a.price)
+    }
+
+    this.setState({
+      filtered: sortedStocks,
+      sortedBy: type
+    })
+  }
   render() {
     return (
       <div>
-        <SearchBar/>
-
+        <SearchBar handleSort={this.handleSort} sortBy={this.sortBy} sortedBy={this.state.sortedBy} />
           <div className="row">
             <div className="col-8">
-
-              <StockContainer/>
-
+              <StockContainer stocks={this.state.filtered} handleStock={this.handleStock} />
             </div>
             <div className="col-4">
-
-              <PortfolioContainer/>
-
+              <PortfolioContainer portfolio={this.state.portfolio} />
             </div>
           </div>
       </div>
